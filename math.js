df947130eff2203c3890e562a053eceaa5f72038
laljanbasha786 <laljanbasha05@gmail.com>
2024-11-05 17:26:10 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,15 @@
+// var str="hello world"
+// console.log(str.replace("world","My dear friend"));
+
+// let a=" hungry "
+// console.log(a.repeat(" 10 "));
+
+// let b = "The quick brown fox jumps over the lazy dog. The fox is clever.";
+// console.log(b.lastIndexOf("fox"));
+
+// let c="hello world"
+// console.log(c.startsWith("hi"));
+
+// let d="document.pdf"
+// console.log(d.endsWith("pdf"));
+
